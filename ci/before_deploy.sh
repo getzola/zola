3ae6865dcb4e40d3a60a460618fb50e9bc9ca731
Vincent Prouillet <vincent@wearewizards.io>
2017-03-24 16:47:20 +0900

    Right name in ci folder

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -18,10 +18,10 @@ main() {
     test -f Cargo.lock || cargo generate-lockfile
 
     # TODO Update this to build the artifacts that matter to you
-    cross rustc --bin hello --target $TARGET --release -- -C lto
+    cross rustc --bin gutenberg --target $TARGET --release -- -C lto
 
     # TODO Update this to package the right artifacts
-    cp target/$TARGET/release/hello $stage/
+    cp target/$TARGET/release/gutenberg $stage/
 
     cd $stage
     tar czf $src/$CRATE_NAME-$TRAVIS_TAG-$TARGET.tar.gz *
