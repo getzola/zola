b58523934821458c583953a76c8016a1ffcf4b0f
Vincent Prouillet <vincent@wearewizards.io>
2017-07-16 10:12:54 +0900

    Disable build.rs now that completions files are built

--- build.rs
+++ build.rs
@@ -6,10 +6,10 @@ use clap::Shell;
 include!("src/cli.rs");
 
 fn main() {
-    let mut app = build_cli();
-    println!("hello");
-    app.gen_completions("gutenberg", Shell::Bash, "completions/");
-    app.gen_completions("gutenberg", Shell::Fish, "completions/");
-    app.gen_completions("gutenberg", Shell::Zsh, "completions/");
-    app.gen_completions("gutenberg", Shell::PowerShell, "completions/");
+    // disabled below as it fails in CI
+//    let mut app = build_cli();
+//    app.gen_completions("gutenberg", Shell::Bash, "completions/");
+//    app.gen_completions("gutenberg", Shell::Fish, "completions/");
+//    app.gen_completions("gutenberg", Shell::Zsh, "completions/");
+//    app.gen_completions("gutenberg", Shell::PowerShell, "completions/");
 }
