204f514c0e2c1f45c99da67af45bb7f29045ac33
Vincent Prouillet <vincent@wearewizards.io>
2018-05-03 20:50:30 +0200

    WIP: new markdown rendering

--- components/rendering/src/lib.rs
+++ components/rendering/src/lib.rs
@@ -8,20 +8,27 @@ extern crate slug;
 #[macro_use]
 extern crate serde_derive;
 extern crate serde;
+extern crate pest;
+#[macro_use]
+extern crate pest_derive;
+
 
 extern crate errors;
 extern crate front_matter;
 extern crate highlighting;
 extern crate utils;
+extern crate config;
 
 #[cfg(test)]
 extern crate templates;
 
 mod context;
 mod markdown;
-mod short_code;
 mod table_of_contents;
+mod shortcode;
+mod short_code;
 
 pub use context::Context;
 pub use markdown::markdown_to_html;
 pub use table_of_contents::Header;
+pub use shortcode::render_shortcodes;
