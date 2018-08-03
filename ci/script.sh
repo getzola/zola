fb6e3bd5cc385943a512367d6819fc732159dc6e
Vincent Prouillet <prouillet.vincent@gmail.com>
2018-08-03 22:34:56 +0200

    Fix CIs

--- ci/script.sh
+++ ci/script.sh
@@ -4,14 +4,12 @@ set -ex
 
 # TODO This is the "test phase", tweak it as you see fit
 main() {
-    cross build --target $TARGET
     cross build --target $TARGET --release
 
     if [ ! -z $DISABLE_TESTS ]; then
         return
     fi
 
-    cross test --all --target $TARGET
     cross test --all --target $TARGET --release
 }
 
