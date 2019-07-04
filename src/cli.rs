301d714b6ef4b5cb48890d26325dcbd0d67189c9
Tim Visée <tim@visee.me>
2019-07-04 23:42:37 +0200

    Add --open flag to serve command, to open URL in default browser (#739)
    * Add --open flag to serve command, to open URL in default browser
    
    * Mention serve --open flag in CLI documentation

--- src/cli.rs
+++ src/cli.rs
@@ -65,7 +65,12 @@ pub fn build_cli() -> App<'static, 'static> {
                     Arg::with_name("watch_only")
                         .long("watch-only")
                         .takes_value(false)
-                        .help("Do not start a server, just re-build project on changes")
+                        .help("Do not start a server, just re-build project on changes"),
+                    Arg::with_name("open")
+                        .short("O")
+                        .long("open")
+                        .takes_value(false)
+                        .help("Open site in the default browser"),
                 ]),
             SubCommand::with_name("check")
                 .about("Try building the project without rendering it. Checks links")
