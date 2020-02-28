2eaa791349d5a91d0ded348c7e38ae2de9d5dd73
Vincent Prouillet <balthek@gmail.com>
2020-02-28 08:17:01 +0100

    Enable strikethrough in markdown filter

--- components/templates/src/filters.rs
+++ components/templates/src/filters.rs
@@ -18,6 +18,7 @@ pub fn markdown<S: BuildHasher>(
     let mut opts = cmark::Options::empty();
     opts.insert(cmark::Options::ENABLE_TABLES);
     opts.insert(cmark::Options::ENABLE_FOOTNOTES);
+    opts.insert(cmark::Options::ENABLE_STRIKETHROUGH);
 
     let mut html = String::new();
     let parser = cmark::Parser::new_ext(&s, opts);
