e47deccf43d3441cee703542cb4634a4d77af3bc
southerntofu <52931252+southerntofu@users.noreply.github.com>
2020-06-18 20:31:03 +0000

    Make themes more flexible (#1004)
    * Site templates can replace theme templates
    
    * Integrate test case within test_site/
    
    * Full backwards-compatibility with testcase in test_site
    
    * Refine test case
    
    * Call parent's block in child template for test case
    
    * Check both templates are applied
    
    * Follow testing advice
    
    * Test for 'include' in themes and shortcodes
    
    * Documentation for themes and how to extend them
    
    Co-authored-by: Vincent Prouillet <balthek@gmail.com>

--- components/utils/src/templates.rs
+++ components/utils/src/templates.rs
@@ -61,52 +61,21 @@ pub fn render_template(
     }
 }
 
-/// Rewrites the path from extend/macros of the theme used to ensure
-/// that they will point to the right place (theme/templates/...)
-/// Include is NOT supported as it would be a pain to add and using blocks
-/// or macros is always better anyway for themes
-/// This will also rename the shortcodes to NOT have the themes in the path
-/// so themes shortcodes can be used.
-pub fn rewrite_theme_paths(tera_theme: &mut Tera, site_templates: Vec<&str>, theme: &str) {
-    let mut shortcodes_to_move = vec![];
-    let mut templates = HashMap::new();
-    let old_templates = ::std::mem::replace(&mut tera_theme.templates, HashMap::new());
-
-    // We want to match the paths in the templates to the new names
-    for (key, mut tpl) in old_templates {
-        tpl.name = format!("{}/templates/{}", theme, tpl.name);
-        // First the parent if there is one
-        // If a template with the same name is also in site, assumes it overrides the theme one
-        // and do not change anything
-        if let Some(ref p) = tpl.parent.clone() {
-            if !site_templates.contains(&p.as_ref()) {
-                tpl.parent = Some(format!("{}/templates/{}", theme, p));
-            }
-        }
-
-        // Next the macros import
-        let mut updated = vec![];
-        for &(ref filename, ref namespace) in &tpl.imported_macro_files {
-            updated.push((format!("{}/templates/{}", theme, filename), namespace.to_string()));
-        }
-        tpl.imported_macro_files = updated;
-
-        if tpl.name.starts_with(&format!("{}/templates/shortcodes", theme)) {
-            let new_name = tpl.name.replace(&format!("{}/templates/", theme), "");
-            shortcodes_to_move.push((key, new_name.clone()));
-            tpl.name = new_name;
-        }
-
-        templates.insert(tpl.name.clone(), tpl);
+/// Rewrites the path of duplicate templates to include the complete theme path
+/// Theme templates  will be injected into site templates, with higher priority for site
+/// templates. To keep a copy of the template in case it's being extended from a site template
+/// of the same name, we reinsert it with the theme path prepended
+pub fn rewrite_theme_paths(tera_theme: &mut Tera, theme: &str) {
+    let theme_basepath = format!("{}/templates/", theme);
+    let mut new_templates = HashMap::new();
+    for (key, template) in &tera_theme.templates {
+        let mut tpl = template.clone();
+        tpl.name = format!("{}{}", theme_basepath, key);
+        new_templates.insert(tpl.name.clone(), tpl);
     }
-
-    tera_theme.templates = templates;
-
-    // and then replace shortcodes in the Tera instance using the new names
-    for (old_name, new_name) in shortcodes_to_move {
-        let tpl = tera_theme.templates.remove(&old_name).unwrap();
-        tera_theme.templates.insert(new_name, tpl);
-    }
+    // Contrary to tera.extend, hashmap.extend does replace existing keys
+    // We can safely extend because there's no conflicting paths anymore
+    tera_theme.templates.extend(new_templates);
 }
 
 #[cfg(test)]
@@ -117,7 +86,7 @@ mod tests {
     #[test]
     fn can_rewrite_all_paths_of_theme() {
         let mut tera = Tera::parse("test-templates/*.html").unwrap();
-        rewrite_theme_paths(&mut tera, vec!["base.html"], "hyde");
+        rewrite_theme_paths(&mut tera, "hyde");
         // special case to make the test work: we also rename the files to
         // match the imports
         for (key, val) in &tera.templates.clone() {
@@ -133,7 +102,7 @@ mod tests {
         );
         assert_eq!(
             tera.templates["hyde/templates/child.html"].parent,
-            Some("hyde/templates/index.html".to_string())
+            Some("index.html".to_string())
         );
     }
 }
