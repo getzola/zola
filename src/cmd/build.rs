ca91d165c40889f63de14d95b926c9167f2872ad
Gijs Burghoorn <g.burghoorn@gmail.com>
2021-07-21 23:54:19 +0200

    Added ask prompt for output-dir flag
    Added `ask_bool` prompt for `--output-dir` for when the output directory
    targeted already exists in the file system.
    
    [Issue: #1378]

--- src/cmd/build.rs
+++ src/cmd/build.rs
@@ -1,9 +1,10 @@
 use std::path::Path;
 
-use errors::Result;
+use errors::{Error, Result};
 use site::Site;
 
 use crate::console;
+use crate::prompt::ask_bool;
 
 pub fn build(
     root_dir: &Path,
@@ -14,6 +15,17 @@ pub fn build(
 ) -> Result<()> {
     let mut site = Site::new(root_dir, config_file)?;
     if let Some(output_dir) = output_dir {
+        // Check whether output directory exists or not
+        // This way we don't replace already existing files.
+        if output_dir.exists() {
+            console::warn("The directory you are about build to already exists. Building to this directory will delete files contained within this directory.");
+
+            // Prompt the user to ask whether they want to continue.
+            if !ask_bool("Are you you want to continue?", false)? {
+                return Err(Error::msg("Cancelled build process because output directory already exists."));
+            }
+        }
+
         site.set_output_path(output_dir);
     }
     if let Some(b) = base_url {
