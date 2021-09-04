eceb1bd79d3b3ab5b3818039aa0d533bb61f3ad7
Gijs Burghoorn <g.burghoorn@gmail.com>
2021-09-04 08:10:02 +0200

    Added prompt for when zola build output-dir mentions an existing directory. (#1558)
    * Next version
    
    * Added ask prompt for output-dir flag
    
    Added `ask_bool` prompt for `--output-dir` for when the output directory
    targeted already exists in the file system.
    
    [Issue: #1378]
    
    * Updated the documentation for #1378
    
    * Added missing "sure" in prompt text
    
    * Added timeout to prompt + dirname
    
    * Fixed complication errors
    
    Co-authored-by: Vincent Prouillet <balthek@gmail.com>

--- src/cmd/build.rs
+++ src/cmd/build.rs
@@ -1,9 +1,12 @@
 use std::path::Path;
 
-use errors::Result;
+use errors::{Error, Result};
 use site::Site;
 
 use crate::console;
+use crate::prompt::ask_bool_timeout;
+
+const BUILD_PROMPT_TIMEOUT_MILLIS: u64 = 10_000;
 
 pub fn build(
     root_dir: &Path,
@@ -14,6 +17,27 @@ pub fn build(
 ) -> Result<()> {
     let mut site = Site::new(root_dir, config_file)?;
     if let Some(output_dir) = output_dir {
+        // Check whether output directory exists or not
+        // This way we don't replace already existing files.
+        if output_dir.exists() {
+            console::warn(&format!("The directory '{}' already exists. Building to this directory will delete files contained within this directory.", output_dir.display()));
+
+            // Prompt the user to ask whether they want to continue.
+            let clear_dir = tokio::runtime::Runtime::new()
+                .expect("Tokio runtime failed to instantiate")
+                .block_on(ask_bool_timeout(
+                    "Are you sure you want to continue?",
+                    false,
+                    std::time::Duration::from_millis(BUILD_PROMPT_TIMEOUT_MILLIS),
+                ))?;
+
+            if !clear_dir {
+                return Err(Error::msg(
+                    "Cancelled build process because output directory already exists.",
+                ));
+            }
+        }
+
         site.set_output_path(output_dir);
     }
     if let Some(b) = base_url {
