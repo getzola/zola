c9b2d35f47891fd4f80aa8223b5f5cf9b78e5ae5
Marcin Puc <5671049+tranzystorek-io@users.noreply.github.com>
2022-01-23 20:21:41 +0100

    Update arg parsing to clap v3 (#1717)
    * Update arg parsing to clap v3

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-use std::env;
-use std::path::{Path, PathBuf};
 use std::time::Instant;
 
+use cli::{Cli, Command};
 use utils::net::{get_available_port, port_is_available};
+
+use clap::Parser;
 
 mod cli;
 mod cmd;
@@ -10,115 +11,82 @@ mod console;
 mod prompt;
 
 fn main() {
-    let matches = cli::build_cli().get_matches();
+    let cli = Cli::parse();
+    let root_dir = cli
+        .root
+        .canonicalize()
+        .unwrap_or_else(|_| panic!("Cannot find root directory: {}", cli.root.display()));
+    let config_file = cli
+        .config
+        .map(|path| {
+            path.canonicalize()
+                .unwrap_or_else(|_| panic!("Cannot find config file: {}", path.display()))
+        })
+        .unwrap_or_else(|| root_dir.join("config.toml"));
 
-    let root_dir = match matches.value_of("root").unwrap() {
-        "." => env::current_dir().unwrap(),
-        path => PathBuf::from(path)
-            .canonicalize()
-            .unwrap_or_else(|_| panic!("Cannot find root directory: {}", path)),
-    };
-    let config_file = match matches.value_of("config") {
-        Some(path) => PathBuf::from(path)
-            .canonicalize()
-            .unwrap_or_else(|_| panic!("Cannot find config file: {}", path)),
-        None => root_dir.join("config.toml"),
-    };
-
-    match matches.subcommand() {
-        ("init", Some(matches)) => {
-            let force = matches.is_present("force");
-            match cmd::create_new_project(matches.value_of("name").unwrap(), force) {
-                Ok(()) => (),
-                Err(e) => {
-                    console::unravel_errors("Failed to create the project", &e);
-                    ::std::process::exit(1);
-                }
-            };
+    match cli.command {
+        Command::Init { name, force } => {
+            if let Err(e) = cmd::create_new_project(&name, force) {
+                console::unravel_errors("Failed to create the proejct", &e);
+                std::process::exit(1);
+            }
         }
-        ("build", Some(matches)) => {
+        Command::Build { base_url, output_dir, drafts } => {
             console::info("Building site...");
             let start = Instant::now();
-            let output_dir = matches.value_of("output_dir").map(|output_dir| Path::new(output_dir));
             match cmd::build(
                 &root_dir,
                 &config_file,
-                matches.value_of("base_url"),
-                output_dir,
-                matches.is_present("drafts"),
+                base_url.as_deref(),
+                output_dir.as_deref(),
+                drafts,
             ) {
                 Ok(()) => console::report_elapsed_time(start),
                 Err(e) => {
                     console::unravel_errors("Failed to build the site", &e);
-                    ::std::process::exit(1);
+                    std::process::exit(1);
                 }
-            };
+            }
         }
-        ("serve", Some(matches)) => {
-            let interface = matches.value_of("interface").unwrap_or("127.0.0.1");
-            let mut port: u16 = match matches.value_of("port").unwrap_or("1111").parse() {
-                Ok(x) => x,
-                Err(_) => {
-                    console::error("The request port needs to be an integer");
-                    ::std::process::exit(1);
-                }
-            };
-            let open = matches.is_present("open");
-            let include_drafts = matches.is_present("drafts");
-            let fast = matches.is_present("fast");
-
-            // Default one
+        Command::Serve { interface, mut port, output_dir, base_url, drafts, open, fast } => {
             if port != 1111 && !port_is_available(port) {
                 console::error("The requested port is not available");
-                ::std::process::exit(1);
+                std::process::exit(1);
             }
 
             if !port_is_available(port) {
-                port = if let Some(p) = get_available_port(1111) {
-                    p
-                } else {
-                    console::error("No port available.");
-                    ::std::process::exit(1);
-                }
+                port = get_available_port(1111).unwrap_or_else(|| {
+                    console::error("No port available");
+                    std::process::exit(1);
+                });
             }
-            let output_dir = matches.value_of("output_dir").map(|output_dir| Path::new(output_dir));
-            let base_url = matches.value_of("base_url").unwrap_or("127.0.0.1");
+
             console::info("Building site...");
-            match cmd::serve(
+            if let Err(e) = cmd::serve(
                 &root_dir,
-                interface,
+                &interface,
                 port,
-                output_dir,
-                base_url,
+                output_dir.as_deref(),
+                &base_url,
                 &config_file,
                 open,
-                include_drafts,
+                drafts,
                 fast,
             ) {
-                Ok(()) => (),
-                Err(e) => {
-                    console::unravel_errors("", &e);
-                    ::std::process::exit(1);
-                }
-            };
+                console::unravel_errors("Failed to serve the site", &e);
+                std::process::exit(1);
+            }
         }
-        ("check", Some(matches)) => {
+        Command::Check { drafts } => {
             console::info("Checking site...");
             let start = Instant::now();
-            match cmd::check(
-                &root_dir,
-                &config_file,
-                matches.value_of("base_path"),
-                matches.value_of("base_url"),
-                matches.is_present("drafts"),
-            ) {
+            match cmd::check(&root_dir, &config_file, None, None, drafts) {
                 Ok(()) => console::report_elapsed_time(start),
                 Err(e) => {
                     console::unravel_errors("Failed to check the site", &e);
-                    ::std::process::exit(1);
+                    std::process::exit(1);
                 }
-            };
+            }
         }
-        _ => unreachable!(),
     }
 }
