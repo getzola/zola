bd85ca41e525601b44ce47af7b4a8ef5a7ed18a5
sinofp <sinofp@tuta.io>
2023-05-06 13:20:47 +0100

    Add test with empty alt and special characters
    I totaly forgot one can leave the alt text empty.
    I thought I need to eliminate the alt attribute in that case,
    but actually empty alt text is better than not having an alt attribute at all:
    https://www.w3.org/TR/WCAG20-TECHS/H67.html
    https://www.boia.org/blog/images-that-dont-need-alternative-text-still-need-alt-attributes
    Thus I will leave the empty alt text.
    
    Another test is added to ensure alt text is properly escaped.
    I will remove the redundant escaping code after this commit.

--- components/markdown/tests/img.rs
+++ components/markdown/tests/img.rs
@@ -7,6 +7,14 @@ fn can_transform_image() {
     let rendered = common::render("![haha](https://example.com/abc.jpg)").unwrap().body;
     assert_eq!(rendered, "<p><img src=\"https://example.com/abc.jpg\" alt=\"haha\" /></p>\n");
 
+    // empty alt text
+    let rendered = common::render("![](https://example.com/abc.jpg)").unwrap().body;
+    assert_eq!(rendered, "<p><img src=\"https://example.com/abc.jpg\" alt=\"\" /></p>\n");
+
+    // alt text needs to be escaped
+    let rendered = common::render("![ha\"h>a](https://example.com/abc.jpg)").unwrap().body;
+    assert_eq!(rendered, "<p><img src=\"https://example.com/abc.jpg\" alt=\"ha&quot;h&gt;a\" /></p>\n");
+
     // alt text with style
     let rendered = common::render("![__ha__*ha*](https://example.com/abc.jpg)").unwrap().body;
     assert_eq!(rendered, "<p><img src=\"https://example.com/abc.jpg\" alt=\"haha\" /></p>\n");
@@ -19,14 +27,28 @@ fn can_transform_image() {
 
 #[test]
 fn can_add_lazy_loading_and_async_decoding() {
-    // normal alt text
     let mut config = Config::default_for_test();
     config.markdown.lazy_async_image = true;
+
+    // normal alt text
     let rendered =
         common::render_with_config("![haha](https://example.com/abc.jpg)", config.clone())
             .unwrap()
             .body;
     assert_eq!(rendered, "<p><img src=\"https://example.com/abc.jpg\" alt=\"haha\" loading=\"lazy\" decoding=\"async\" /></p>\n");
+
+    // empty alt text
+    let rendered = common::render_with_config("![](https://example.com/abc.jpg)", config.clone())
+        .unwrap()
+        .body;
+    assert_eq!(rendered, "<p><img src=\"https://example.com/abc.jpg\" alt=\"\" loading=\"lazy\" decoding=\"async\" /></p>\n");
+
+    // alt text needs to be escaped
+    let rendered =
+        common::render_with_config("![ha\"h>a](https://example.com/abc.jpg)", config.clone())
+            .unwrap()
+            .body;
+    assert_eq!(rendered, "<p><img src=\"https://example.com/abc.jpg\" alt=\"ha&quot;h&gt;a\" loading=\"lazy\" decoding=\"async\" /></p>\n");
 
     // Below is acceptable, but not recommended by CommonMark
 
