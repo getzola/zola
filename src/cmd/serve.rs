e81e139cbc2ac7dfaed6c487250bc1d6842262a0
SquirrelHub <SquirrelHub@users.noreply.github.com>
2023-08-17 08:26:34 -0500

    Add Checking and Force Flag for Directory in Serve Command (#2265)
    * Introduce option to force directory when running the serve command
    
    * Update documentation about the force flag on the serve command
    
    * Resolve cargo fmt issue
    
    * Reword new serve flag documentation

--- src/cmd/serve.rs
+++ src/cmd/serve.rs
@@ -43,7 +43,7 @@ use libs::serde_json;
 use notify::{watcher, RecursiveMode, Watcher};
 use ws::{Message, Sender, WebSocket};
 
-use errors::{anyhow, Context, Result};
+use errors::{anyhow, Context, Error, Result};
 use pathdiff::diff_paths;
 use site::sass::compile_sass;
 use site::{Site, SITE_CONTENT};
@@ -246,6 +246,7 @@ fn create_new_site(
     interface: &str,
     interface_port: u16,
     output_dir: Option<&Path>,
+    force: bool,
     base_url: &str,
     config_file: &Path,
     include_drafts: bool,
@@ -276,6 +277,12 @@ fn create_new_site(
     site.enable_serve_mode();
     site.set_base_url(base_url);
     if let Some(output_dir) = output_dir {
+        if !force && output_dir.exists() {
+            return Err(Error::msg(format!(
+                "Directory '{}' already exists. Use --force to overwrite.",
+                output_dir.display(),
+            )));
+        }
         site.set_output_path(output_dir);
     }
     if include_drafts {
@@ -299,6 +306,7 @@ pub fn serve(
     interface: &str,
     interface_port: u16,
     output_dir: Option<&Path>,
+    force: bool,
     base_url: &str,
     config_file: &Path,
     open: bool,
@@ -313,6 +321,7 @@ pub fn serve(
         interface,
         interface_port,
         output_dir,
+        force,
         base_url,
         config_file,
         include_drafts,
@@ -521,6 +530,7 @@ pub fn serve(
         interface,
         interface_port,
         output_dir,
+        force,
         base_url,
         config_file,
         include_drafts,
