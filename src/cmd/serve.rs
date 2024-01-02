getzola/zola

commit fe1967fb0fe063b1cee1ad48820870ab2ecc0e5b

Fix LFI in `zola serve` (#2258)
* use fs canonicalize to prevent path traversal

* fix cargo fmt

src/cmd/serve.rs
@@ -116,6 +116,14 @@ async fn handle_request(req: Request<Body>, mut root: PathBuf) -> Result<Respons
     // otherwise `PathBuf` will interpret it as an absolute path
     root.push(&decoded[1..]);
 
+    // Resolve the root + user supplied path into the absolute path
+    // this should hopefully remove any path traversals
+    // if we fail to resolve path, we should return 404
+    root = match tokio::fs::canonicalize(&root).await {
+        Ok(d) => d,
+        Err(_) => return Ok(not_found()),
+    };
+
     // Ensure we are only looking for things in our public folder
     if !root.starts_with(original_root) {
         return Ok(not_found());

commit df19f5ec0a520a70a5a6c62fdad18fe745e2013f

fix(serve): content can be served from output_path
This fixes a bug introduced in https://github.com/getzola/zola/pull/2258

The issue arose when `output_path` was relative. The request being
served would be canonicalized and this would be a string. So, for
example, if you were serving content from `public` the code
[right after](https://github.com/getzola/zola/blob/38199c125501e9ff0e700e96adaca72cc3f25d2b/src/cmd/serve.rs#L144-L147)
the canonicalization checking if
`root.starts_with(original_root)` would always return `false` since
an absolute path, `/some/path/to/content` would never start with a
string like `public`.

src/cmd/serve.rs
@@ -461,9 +461,9 @@ pub fn serve(
     let ws_address = format!("{}:{}", interface, ws_port.unwrap());
     let output_path = site.output_path.clone();
 
-    // output path is going to need to be moved later on, so clone it for the
-    // http closure to avoid contention.
-    let static_root = output_path.clone();
+    // static_root needs to be canonicalized because we do the same for the http server.
+    let static_root = std::fs::canonicalize(&output_path).unwrap();
+
     let broadcaster = {
         thread::spawn(move || {
             let addr = address.parse().unwrap();