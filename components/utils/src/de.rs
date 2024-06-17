f5bc286ab57183d69a8ba48bcfffd2385f2caf05
Andrew Langmeier <raymi306@gmail.com>
2024-06-16 21:19:02 -0400

    Wire things up; Add more tests

--- components/utils/src/de.rs
+++ components/utils/src/de.rs
@@ -1,15 +1,16 @@
 use core::convert::TryFrom;
-use errors::Result;
+use errors::{Result, anyhow};
 use libs::regex::Regex;
 use libs::tera::{Map, Value};
 use libs::time;
+use libs::time::format_description::well_known::Rfc3339;
 use libs::toml;
 use serde::{Deserialize, Deserializer};
 
 pub fn parse_yaml_datetime(date_string: &str) -> Result<time::OffsetDateTime> {
     // See https://github.com/getzola/zola/issues/2071#issuecomment-1530610650
-    let re = Regex::new(r#"^"?([0-9]{4})-([0-9][0-9]?)-([0-9][0-9]?)([Tt]|[ \t]+)([0-9][0-9]?):([0-9]{2}):([0-9]{2})(\.[0-9]*)?Z?([ \t]([-+][0-9][0-9]?)(:([0-9][0-9]?))?Z?|([-+][0-9]{2})?:([0-9]{2})?)?|([0-9]{4})-([0-9]{2})-([0-9]{2})"?$"#).unwrap();
-    let captures = re.captures(date_string).unwrap();
+    let re = Regex::new(r#"^"?([0-9]{4})-([0-9][0-9]?)-([0-9][0-9]?)([Tt]|[ \t]+)([0-9][0-9]?):([0-9]{2}):([0-9]{2})\.([0-9]*)?Z?([ \t]([-+][0-9][0-9]?)(:([0-9][0-9]?))?Z?|([-+][0-9]{2})?:([0-9]{2})?)?|([0-9]{4})-([0-9]{2})-([0-9]{2})"?$"#).unwrap();
+    let captures = if let Some(captures_) = re.captures(date_string) { Ok(captures_) } else { Err(anyhow!("Error parsing YAML datetime")) }?;
     let year =
         if let Some(cap) = captures.get(1) { cap } else { captures.get(15).unwrap() }.as_str();
     let month =
@@ -19,7 +20,15 @@ pub fn parse_yaml_datetime(date_string: &str) -> Result<time::OffsetDateTime> {
     let hours = if let Some(hours_) = captures.get(5) { hours_.as_str() } else { "0" };
     let minutes = if let Some(minutes_) = captures.get(6) { minutes_.as_str() } else { "0" };
     let seconds = if let Some(seconds_) = captures.get(7) { seconds_.as_str() } else { "0" };
-    //let fractional_seconds = captures.get(8);
+    let fractional_seconds_raw = if let Some(fractionals) = captures.get(8) { fractionals.as_str() } else { "" };
+    let fractional_seconds_intermediate = fractional_seconds_raw.trim_end_matches("0");
+    //
+    // Prepare for eventual conversion into nanoseconds
+    let fractional_seconds = if fractional_seconds_intermediate.len() > 0 && fractional_seconds_intermediate.len() <= 9 {
+        fractional_seconds_intermediate
+    } else {
+        "0"
+    };
     let maybe_timezone_hour_1 = captures.get(10);
     let maybe_timezone_minute_1 = captures.get(12);
     let maybe_timezone_hour_2 = captures.get(13);
@@ -46,22 +55,22 @@ pub fn parse_yaml_datetime(date_string: &str) -> Result<time::OffsetDateTime> {
         )?);
     }
 
-    // free parse unwraps since we know they're digits courtesy of regex
-    // TODO the heck are the timezone unwraps doing
+    // Free parse unwraps since we know everything is a digit courtesy of prior regex.
     Ok(offset_datetime
         .replace_year(year.parse().unwrap())?
         .replace_month(time::Month::try_from(month.parse::<u8>().unwrap())?)?
         .replace_day(day.parse().unwrap())?
         .replace_hour(hours.parse().unwrap())?
         .replace_minute(minutes.parse().unwrap())?
-        .replace_second(seconds.parse().unwrap())?)
+        .replace_second(seconds.parse().unwrap())?
+        .replace_nanosecond(fractional_seconds.parse::<u32>().unwrap() * 100_000_000)?)
 }
 
 /// Used as an attribute when we want to convert from TOML to a string date
 /// If a TOML datetime isn't present, it will accept a string and push it through
 /// TOML's date time parser to ensure only valid dates are accepted.
 /// Inspired by this proposal: <https://github.com/alexcrichton/toml-rs/issues/269>
-pub fn from_toml_datetime<'de, D>(deserializer: D) -> Result<Option<String>, D::Error>
+pub fn from_unknown_datetime<'de, D>(deserializer: D) -> Result<Option<String>, D::Error>
 where
     D: Deserializer<'de>,
 {
@@ -80,9 +89,14 @@ where
         MaybeDatetime::String(s) => {
             if let Ok(d) = toml::value::Datetime::from_str(&s) {
                 Ok(Some(d.to_string()))
+            } else if let Ok(d) = parse_yaml_datetime(&s) {
+                // Ensure that the resulting string is easily reparseable down the line.
+                // In content::front_matter::page.rs where these strings are currently used,
+                // Rfc3339 works with the explicit demands in that code but not always with the result of
+                // _to_string.
+                Ok(Some(d.format(&Rfc3339).unwrap()))
             } else {
-                // now let's check if the yaml_regex matches
-                Err(D::Error::custom("FIXME"))
+                Err(D::Error::custom("Unable to parse datetime"))
             }
         }
     }
@@ -152,13 +166,46 @@ mod tests {
         let space_separated = "2001-12-14 21:59:43.10 -5";
         let no_time_zone = "2001-12-15 2:59:43.10";
         let date = "2002-12-14";
-        assert_eq!(parse_yaml_datetime(canonical).unwrap(), datetime!(2001-12-15 2:59:43 +0));
-        assert_eq!(parse_yaml_datetime(valid_iso8601).unwrap(), datetime!(2001-12-14 21:59:43 -5));
+        assert_eq!(parse_yaml_datetime(canonical).unwrap(), datetime!(2001-12-15 2:59:43.1 +0));
+        assert_eq!(parse_yaml_datetime(valid_iso8601).unwrap(), datetime!(2001-12-14 21:59:43.1 -5));
         assert_eq!(
             parse_yaml_datetime(space_separated).unwrap(),
-            datetime!(2001-12-14 21:59:43 -5)
+            datetime!(2001-12-14 21:59:43.1 -5)
         );
-        assert_eq!(parse_yaml_datetime(no_time_zone).unwrap(), datetime!(2001-12-15 2:59:43 +0));
+        assert_eq!(parse_yaml_datetime(no_time_zone).unwrap(), datetime!(2001-12-15 2:59:43.1 +0));
         assert_eq!(parse_yaml_datetime(date).unwrap(), datetime!(2002-12-14 0:00:00 +0));
+    }
+    
+    #[test]
+    fn yaml_spec_invalid_dates_fail() {
+        let invalid_month = "2001-13-15";
+        assert!(parse_yaml_datetime(invalid_month).is_err());
+
+        let invalid_month = "2001-13-15T02:59:43.1Z";
+        assert!(parse_yaml_datetime(invalid_month).is_err());
+
+        let no_digits_in_year = "xxxx-12-15";
+        assert!(parse_yaml_datetime(no_digits_in_year).is_err());
+
+        let no_digits_in_year = "xxxx-12-15T02:59:43.1Z";
+        assert!(parse_yaml_datetime(no_digits_in_year).is_err());
+
+        let no_digits_in_month = "2001-xx-15";
+        assert!(parse_yaml_datetime(no_digits_in_month).is_err());
+
+        let no_digits_in_month = "2001-xx-15T02:59:43.1Z";
+        assert!(parse_yaml_datetime(no_digits_in_month).is_err());
+
+        let no_digits_in_day = "2001-12-xx";
+        assert!(parse_yaml_datetime(no_digits_in_day).is_err());
+
+        let no_digits_in_day = "2001-12-xx:59:43.1Z";
+        assert!(parse_yaml_datetime(no_digits_in_day).is_err());
+
+        let unparseable_time = "2001-12-15:69:43.1Z";
+        assert!(parse_yaml_datetime(unparseable_time).is_err());
+
+        let unparseable_time = "2001-12-15:59:4x.1Z";
+        assert!(parse_yaml_datetime(unparseable_time).is_err());
     }
 }
