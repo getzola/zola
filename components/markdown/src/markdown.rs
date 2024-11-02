8f7414d87f75675a23caa462d7b4cf17a369c7eb
William Breathitt Gray <wbg@ishicorp.com>
2024-11-02 22:25:12 +0900

    Improve accessibility by nesting bottom footnotes inside footer element
    This adds semantic meaning to the footnotes organization and improves
    accessibility by aiding disabled users who rely on assistive devices
    such as screen readers which utilize semantic tags such as the footer
    element in order to navigate properly.
    
    The hr element is semantically defined as representing a paragraph-level
    thematic break. Now that the footnotes are descendants of the footer
    element, the hr element originally preceding the footnotes list is
    no longer necessary (footnotes are no longer paragraph-level content)
    and thus replaced. However, the footer element is given the class
    "footnotes" to allow for styling, so the following CSS could be used to
    provide a stylistically equivalent visible border separating the
    footnotes from the content if so desired:
    
        .footnotes {
          border-top: 2px groove gray;
        }
    
    Test snapshots are also updated to reflect the new footer elements.

--- components/markdown/src/markdown.rs
+++ components/markdown/src/markdown.rs
@@ -304,7 +304,8 @@ fn convert_footnotes_to_github_style(old_events: &mut Vec<Event>) {
         return;
     }
 
-    old_events.push(Event::Html("<hr><ol class=\"footnotes-list\">\n".into()));
+    old_events
+        .push(Event::Html("<footer class=\"footnotes\">\n<ol class=\"footnotes-list\">\n".into()));
 
     // Step 2: retain only footnotes which was actually referenced
     footnotes.retain(|f| match f.first() {
@@ -394,7 +395,7 @@ fn convert_footnotes_to_github_style(old_events: &mut Vec<Event>) {
     });
 
     old_events.extend(footnotes);
-    old_events.push(Event::Html("</ol>\n".into()));
+    old_events.push(Event::Html("</ol>\n</footer>\n".into()));
 }
 
 pub fn markdown_to_html(
