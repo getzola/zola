336533dd2944709a1195eac91a8214cbb4a1a7e6
Aritz Beobide-Cardinal <aritz@aritzcracker.ca>
2025-01-02 14:50:57 -0500

    Allow nested shortcodes (#2748)
    * Add ShortcodeInvocationCounter
    
    * Add nested shortcode parsing and rendering
    
    * Remove ShortcodeInvocationCounter from render context
    
    * Remove interior mutability in ShortcodeInvocationCounter
    
    * Rename Shortcode::flatten to render_inner_shortcodes

--- components/markdown/benches/all.rs
+++ components/markdown/benches/all.rs
@@ -147,6 +147,7 @@ fn bench_render_content_with_emoji(b: &mut test::Bencher) {
     let tera = Tera::default();
     let content2 = CONTENT.replace(r#"{{ youtube(id="my_youtube_id") }}"#, "");
     let mut config = Config::default_for_test();
+
     config.markdown.highlight_code = false;
     config.markdown.render_emoji = true;
     let permalinks_ctx = HashMap::new();
