e32fde784afe9576805870715618bbc5fdb7f34b
kleines Filmröllchen <kleines@filmroellchen.eu>
2025-12-14 12:45:03 +0100

    Implement basic env_logger
    As per discussion, this only prints the log level
    with basic coloring as well as the message.
    
    Additionally, replace termcolor by
    anstyle+anstream which provides accurate terminal
    detection and more formatting capabilities.
    (anstyle is already a dependency, and termcolor is
    mainly present in some older package versions.)

--- components/console/src/lib.rs
+++ components/console/src/lib.rs
@@ -1,56 +1,27 @@
-use std::env;
 use std::io::Write;
 
-use once_cell::sync::Lazy;
-use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
-
-/// Termcolor color choice.
-/// We do not rely on ColorChoice::Auto behavior
-/// as the check is already performed by has_color.
-static COLOR_CHOICE: Lazy<ColorChoice> =
-    Lazy::new(|| if has_color() { ColorChoice::Always } else { ColorChoice::Never });
+use anstream::{
+    AutoStream,
+    stream::{AsLockedWrite, RawStream},
+};
+use anstyle::{AnsiColor, Color, Style};
 
 pub fn info(message: &str) {
-    colorize(message, ColorSpec::new().set_bold(true), StandardStream::stdout(*COLOR_CHOICE));
-}
-
-pub fn warn(message: &str) {
-    colorize(
-        &format!("{}{}", "Warning: ", message),
-        ColorSpec::new().set_bold(true).set_fg(Some(Color::Yellow)),
-        StandardStream::stdout(*COLOR_CHOICE),
-    );
+    colorize(message, &Style::new().bold(), AutoStream::auto(std::io::stdout()));
 }
 
 pub fn success(message: &str) {
     colorize(
         message,
-        ColorSpec::new().set_bold(true).set_fg(Some(Color::Green)),
-        StandardStream::stdout(*COLOR_CHOICE),
-    );
-}
-
-pub fn error(message: &str) {
-    colorize(
-        &format!("{}{}", "Error: ", message),
-        ColorSpec::new().set_bold(true).set_fg(Some(Color::Red)),
-        StandardStream::stderr(*COLOR_CHOICE),
+        &Style::new().bold().fg_color(Some(Color::Ansi(AnsiColor::Green))),
+        AutoStream::auto(std::io::stdout()),
     );
 }
 
 /// Print a colorized message to stdout
-fn colorize(message: &str, color: &ColorSpec, mut stream: StandardStream) {
-    stream.set_color(color).unwrap();
-    write!(stream, "{}", message).unwrap();
-    stream.set_color(&ColorSpec::new()).unwrap();
-    writeln!(stream).unwrap();
-}
-
-/// Check whether to output colors
-fn has_color() -> bool {
-    let use_colors = env::var("CLICOLOR").unwrap_or_else(|_| "1".to_string()) != "0"
-        && env::var("NO_COLOR").is_err();
-    let force_colors = env::var("CLICOLOR_FORCE").unwrap_or_else(|_| "0".to_string()) != "0";
-
-    force_colors || use_colors && atty::is(atty::Stream::Stdout)
+fn colorize<S>(message: &str, color: &Style, mut stream: AutoStream<S>)
+where
+    S: RawStream + AsLockedWrite,
+{
+    writeln!(stream, "{color}{}{color:#}", message).unwrap();
 }
