6ac31e834daa244782ac4a89b63f3c582cdb3138
Vincent Prouillet <balthek@gmail.com>
2026-08-09 12:43:54 +0200

    Try to make Windows tests fail (#3228)
    * Try to make Windows tests fail
    
    * Try to fix it now

--- components/site/tests/invalid.rs
+++ components/site/tests/invalid.rs
@@ -1,13 +1,11 @@
 mod common;
 
+use common::test_site_path;
 use site::Site;
-use std::env;
 
 #[test]
 fn errors_on_index_md_page_in_section() {
-    let mut path = env::current_dir().unwrap().parent().unwrap().parent().unwrap().to_path_buf();
-    path.push("test_sites_invalid");
-    path.push("indexmd");
+    let path = test_site_path("test_sites_invalid").join("indexmd");
     let config_file = path.join("config.toml");
     let mut site = Site::new(&path, &config_file).unwrap();
     let res = site.load();
